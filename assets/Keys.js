c644d4a7efc5e96f3590ebdcfa30c7041289a79b
DiegoTorresGutierrez <111834274+DiegoTorresGutierrez@users.noreply.github.com>
2022-11-17 11:26:25 -0600

    Add files via upload

---
+++ assets/Keys.js
@@ -0,0 +1,4 @@
+export default {
+    public: "pk_test_51M4gTCIcEMJ118FpHnxyGKRMiOFD4xEROWpjd3QTMuWGfCH8iJ7j4dcsuXHBXiQMowaq2Mz4UdenDsqXdeeZvSqh00y5eiVS48",
+    secret: "sk_test_51M4gTCIcEMJ118FpMlmuiypXHmo9WWbAtxTQvvObZAE7gLdY95rvUSuGCei0r0UIX9YJHfvBzcbTMtuNXciNrNlp00uno3wF3D"
+  }
